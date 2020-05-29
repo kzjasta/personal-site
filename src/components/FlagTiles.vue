77c131227cd0ec73fe46f45b4e6e766e53f9e259
Kieran Small <kzjasta@gmail.com>
2020-05-29 17:50:45 +0100

    added flag icons and FlagTiles component

---
+++ src/components/FlagTiles.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent">
+      <div class="tile is-child">
+        <inline-svg
+          class="flag-icon"
+          :key="country"
+          v-for="country in countries"
+          :src="require(`@/assets/icons/flags/${country}.svg`)"
+          height="75"
+          width="75"
+        ></inline-svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InlineSvg from "vue-inline-svg";
+export default {
+  components: {
+    InlineSvg
+  },
+  data() {
+    return {
+      countries: [
+        "belgium",
+        "bosnia",
+        "cambodia",
+        "canada",
+        "china",
+        "colombia",
+        "croatia",
+        "cyprus",
+        "denmark",
+        "england",
+        "france",
+        "germany",
+        "greece",
+        "hungary",
+        "ireland",
+        "italy",
+        "laos",
+        "myanmar",
+        "netherlands",
+        "north-korea",
+        "poland",
+        "portugal",
+        "scotland",
+        "serbia",
+        "slovenia",
+        "spain",
+        "sweden",
+        "thailand",
+        "ukraine",
+        "usa",
+        "vietnam",
+        "wales"
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.flag-icon {
+  margin-right: 10px;
+}
+</style>
--- src/views/Travel.vue
+++ src/views/Travel.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div class="columns">
+    <div class="column is-half is-offset-one-quarter">
+      <div class="platform">
+        <h1 class="title">Places I've visited</h1>
+        <FlagTiles></FlagTiles>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import FlagTiles from "@/components/FlagTiles";
+
+export default {
+  components: {
+    FlagTiles
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flag-icon {
+  margin: 5px;
+}
+</style>
